1d387568f3c448c483c6582804920ac381fb6a4f
Anstrum <nanaka.kumiho@gmail.com>
2022-05-15 23:47:02 +0200

    Starting to make logic behind auth system

--- app/src/main/java/com/nanaka/status/authentification/LoginFragment.kt
+++ app/src/main/java/com/nanaka/status/authentification/LoginFragment.kt
@@ -5,13 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputEditText
 import com.nanaka.status.R
 import com.nanaka.status.services.Navigation
 
 class LoginFragment : Fragment() {
     private lateinit var appBar: MaterialToolbar
+
+    private lateinit var inputUsername: TextInputEditText
+    private lateinit var inputMail: TextInputEditText
+    private lateinit var inputPassword: TextInputEditText
+    private lateinit var registerButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
